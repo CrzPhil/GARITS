02c558a189b7733a09e466e31acde125828cdc8c
CrzPhil <philippos.g@me.com>
2022-03-29 21:28:16 +0100

    Implemented Administrator class

--- src/Accounts/Administrator.java
+++ src/Accounts/Administrator.java
@@ -2,6 +2,9 @@ package Accounts;
 
 public class Administrator extends Accounts.User {
 
+	private int hourlyrate;
+
+	// Administrator method to create a new User - To be used in Account management
 	public Accounts.User createUserAccount(long userID, String username, String email, String password, String name) {
 		return new User(userID, username, email, password, name);
 	}
@@ -21,8 +24,35 @@ public class Administrator extends Accounts.User {
 		throw new UnsupportedOperationException();
 	}
 
-	public Administrator(long userID, String username, String email, String password, String name) {
+	// toString method in order for the admin's names to be displayed in the JTree
+	@Override
+	public String toString() {
+		return "Administrator{" +
+				"name='" + name + '\'' +
+				'}';
+	}
+
+	// Debug method to print account details
+	public void printAccount() {
+		System.out.println("[DEBUG]");
+		System.out.println(this.userID);
+		System.out.println(this.name);
+		System.out.println(this.email);
+		System.out.println(this.password);
+		System.out.println(this.hourlyrate);
+	}
+
+	public Administrator(long userID, String username, String email, String password, String name, int hourlyrate) {
 		super(userID, username, email, password, name);
+		this.hourlyrate = hourlyrate;
+	}
+
+	public int getHourlyrate() {
+		return hourlyrate;
+	}
+
+	public void setHourlyrate(int hourlyrate) {
+		this.hourlyrate = hourlyrate;
 	}
 
 }
