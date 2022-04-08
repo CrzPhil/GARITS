003767922ac2dc9c3c72ccb63861bc734bd1564b
ammaarM <71775031+ammaarM@users.noreply.github.com>
2022-04-08 16:49:54 +0100

    data from job pages goes to database

--- src/CreateJobGUI.java
+++ src/CreateJobGUI.java
@@ -31,7 +31,29 @@ public class CreateJobGUI extends JFrame{
         finishButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //SQL_JobHelper.sendData();
+
+                SQL_JobHelper sqlJob = new SQL_JobHelper();
+                String jobType = textField3.getText();
+                float duration = Float.parseFloat(textField4.getText());
+                String dates = textField2.getText();
+                String parts = textField5.getText();
+                String motNo = textField6.getText();
+                int mileage = Integer.parseInt(textField7.getText());
+                float price = Float.parseFloat(textField1.getText());
+                String requiredParts = textField5.getText();
+                String additionalInfo = textArea1.getText();
+
+                System.out.println(jobType);
+                System.out.println(duration);
+                System.out.println(dates);
+                System.out.println(parts);
+                System.out.println(motNo);
+                System.out.println(mileage);
+                System.out.println(price);
+                System.out.println(requiredParts);
+                System.out.println(additionalInfo);
+
+                sqlJob.sendData(jobType, duration, dates, parts, motNo, mileage, price, requiredParts, additionalInfo);
                 j.dispose();
                 JobsMenuGUI.main();
             }
--- src/Job/SQL_JobHelper.java
+++ src/Job/SQL_JobHelper.java
@@ -22,14 +22,15 @@ public class SQL_JobHelper extends Database_Controller {
 		return this.query;
 	}
 
-	public static Job[] sendData(String jobType, double duration, String dates, String parts, String motNO, double mileage, double price){
-		String sendJob = String.format("INSERT INTO Jobs (jobType, duration, dates, parts, motNo, mileage, price)");
-		String sendValues = String.format("VALUES ,'%s', '%f', '%s', '%s', '%s', '%d', '%f'");
+	public Job[] sendData(String jobType, float duration, String dates, String parts, String motNO, int mileage, float price, String requiredParts, String additionalInfo){
+		String sendJob = String.format("INSERT INTO Jobs (jobType, duration, dates, parts, motNo, mileage, price, requiredParts, additionalInfo)");
+		String sendValues = String.format(" VALUES ('%s', '%f', '%s', '%s', '%s', '%d', '%f', '%s', '%s')", jobType, duration, dates, parts, motNO, mileage, price, requiredParts, additionalInfo);
 		Job[] out = null;
 
 		try {
 			Statement st = conn.createStatement();
-
+			System.out.println("cas");
+			st.executeUpdate(sendJob + sendValues);
 
 		} catch (SQLException ex) {
 			System.err.println(ex.getMessage());
