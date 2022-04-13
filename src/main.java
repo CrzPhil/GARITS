4d2bc01b89c01bf12944df07b36300619ade51b7
charliejacobs98 <72016216+charliejacobs98@users.noreply.github.com>
2022-04-13 19:37:35 +0100

    job report screen updated

--- src/main.java
+++ src/main.java
@@ -9,6 +9,8 @@ public class main {
         //CustomerAccountMenuGUI.main();
         //UserAccountMenuGUI.main();
         //CreateJobGUI.main();
-        ReportsMenuGUI.main();
+        //ReportsMenuGUI.main();
+        //ManageDatabaseGUI.main();
+        JobReportGUI.main();
     }
 }
